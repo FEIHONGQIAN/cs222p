06ce941f20fe953d2cd1bce725c4e7f4c9f71be9
hanroro <songhan1994@gmail.com>
2020-01-19 17:33:32 -0800

    add conditions to deal with multiple bytes

--- rbf/rbfm.cc
+++ rbf/rbfm.cc
@@ -1,6 +1,7 @@
 #include "rbfm.h"
 
-RecordBasedFileManager &RecordBasedFileManager::instance() {
+RecordBasedFileManager &RecordBasedFileManager::instance()
+{
     static RecordBasedFileManager _rbf_manager = RecordBasedFileManager();
     return _rbf_manager;
 }
@@ -13,66 +14,81 @@ RecordBasedFileManager::RecordBasedFileManager(const RecordBasedFileManager &) =
 
 RecordBasedFileManager &RecordBasedFileManager::operator=(const RecordBasedFileManager &) = default;
 
-RC RecordBasedFileManager::createFile(const std::string &fileName) {
+RC RecordBasedFileManager::createFile(const std::string &fileName)
+{
     // return -1;
-    if (PagedFileManager::instance().createFile(fileName) != 0) {
+    if (PagedFileManager::instance().createFile(fileName) != 0)
+    {
         return -1;
     }
-    else {
-        return 0;
-    }
-
-}
-
-RC RecordBasedFileManager::destroyFile(const std::string &fileName) {
-    // return -1;
-    if (PagedFileManager::instance().destroyFile(fileName) != 0) {
-        return -1;
-    }
-    else {
+    else
+    {
         return 0;
     }
 }
 
-RC RecordBasedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle) {
+RC RecordBasedFileManager::destroyFile(const std::string &fileName)
+{
     // return -1;
-    if (PagedFileManager::instance().openFile(fileName, fileHandle) != 0) {
+    if (PagedFileManager::instance().destroyFile(fileName) != 0)
+    {
         return -1;
     }
-    else {
+    else
+    {
         return 0;
     }
 }
 
-RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
+RC RecordBasedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle)
+{
     // return -1;
-    if (PagedFileManager::instance().closeFile(fileHandle) != 0) {
+    if (PagedFileManager::instance().openFile(fileName, fileHandle) != 0)
+    {
         return -1;
     }
-    else {
+    else
+    {
+        return 0;
+    }
+}
+
+RC RecordBasedFileManager::closeFile(FileHandle &fileHandle)
+{
+    // return -1;
+    if (PagedFileManager::instance().closeFile(fileHandle) != 0)
+    {
+        return -1;
+    }
+    else
+    {
         return 0;
     }
 }
 
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
-                                        const void *data, RID &rid) {
+                                        const void *data, RID &rid)
+{
     return -1;
 }
 
 RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
-                                      const RID &rid, void *data) {
+                                      const RID &rid, void *data)
+{
     return -1;
 }
 
 RC RecordBasedFileManager::deleteRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
-                                        const RID &rid) {
+                                        const RID &rid)
+{
     return -1;
 }
-RC RecordBasedFileManager::getActualByteForNullsIndicator(int fieldCount) {
-        return ceil((double) fieldCount / CHAR_BIT);
-
+RC RecordBasedFileManager::getActualByteForNullsIndicator(int fieldCount)
+{
+    return ceil((double)fieldCount / CHAR_BIT);
 }
-RC RecordBasedFileManager::printRecord(const std::vector<Attribute> &recordDescriptor, const void *data) {
+RC RecordBasedFileManager::printRecord(const std::vector<Attribute> &recordDescriptor, const void *data)
+{
     // return -1;
     bool nullBit = false;
 
@@ -83,23 +99,27 @@ RC RecordBasedFileManager::printRecord(const std::vector<Attribute> &recordDescr
 
     auto *nullFieldsIndicator = (unsigned char *)malloc(sizeof(char) * nullFieldsIndicatorActualSize);
     memset(nullFieldsIndicator, 0, nullFieldsIndicatorActualSize);
-    memcpy((char *) nullFieldsIndicator, data, nullFieldsIndicatorActualSize);
+    memcpy((char *)nullFieldsIndicator, data, nullFieldsIndicatorActualSize);
     offset += nullFieldsIndicatorActualSize;
 
     int leftShift = 7;
     // std::cout << "gogo" << std::endl;
-    for (int i = 0; i < recordDescriptor.size(); i++) {
+    for (int i = 0; i < recordDescriptor.size(); i++)
+    {
         Attribute attr = recordDescriptor[i];
 
-        if (nullFieldsIndicator[i / 8] & ((unsigned) 1 << (unsigned) leftShift)) {
-            std::cout << attr.name << ": NULL" << "\t";
+        if (nullFieldsIndicator[i / 8] & ((unsigned)1 << (unsigned)leftShift))
+        {
+            std::cout << attr.name << ": NULL"
+                      << "\t";
         }
-        else {
+        else
+        {
             void *buffer;
             switch (attr.type)
             {
             case TypeInt:
-                
+
                 buffer = malloc(sizeof(int));
                 memcpy(buffer, (char *)data + offset, sizeof(int));
                 offset += sizeof(int);
@@ -112,46 +132,52 @@ RC RecordBasedFileManager::printRecord(const std::vector<Attribute> &recordDescr
                 std::cout << attr.name << ": " << (*(float *)buffer) << "\t";
                 break;
             case TypeVarChar:
-                    buffer = malloc(sizeof(int));
-                    memcpy(buffer, (char *) data + offset, sizeof(int));
-                    int varCharLen = *(int *) buffer;
-                    offset += sizeof(int);
-                    free(buffer);
-                    buffer = malloc(varCharLen);
-                    memcpy((char *) buffer, (char *) data + offset, varCharLen);
-                    offset += varCharLen;
+                buffer = malloc(sizeof(int));
+                memcpy(buffer, (char *)data + offset, sizeof(int));
+                int varCharLen = *(int *)buffer;
+                offset += sizeof(int);
+                free(buffer);
+                buffer = malloc(varCharLen);
+                memcpy((char *)buffer, (char *)data + offset, varCharLen);
+                offset += varCharLen;
 
-                    std::cout << attr.name << ": ";
-                    auto *tempPointer = (char *) buffer;
-                    for (int i = 0; i < varCharLen; i++) {
-                        std :: cout << *tempPointer;
-                        tempPointer++;
-                    }
-                    std :: cout << "\t";
-                    break;
+                std::cout << attr.name << ": ";
+                auto *tempPointer = (char *)buffer;
+                for (int i = 0; i < varCharLen; i++)
+                {
+                    std ::cout << *tempPointer;
+                    tempPointer++;
+                }
+                std ::cout << "\t";
+                break;
             }
             free(buffer);
         }
+        if (leftShift == 0)
+        {
+            leftShift += 8;
+        }
+        leftShift--;
     }
     std::cout << std::endl;
     return 0;
 }
 
 RC RecordBasedFileManager::updateRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
-                                        const void *data, const RID &rid) {
+                                        const void *data, const RID &rid)
+{
     return -1;
 }
 
 RC RecordBasedFileManager::readAttribute(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
-                                         const RID &rid, const std::string &attributeName, void *data) {
+                                         const RID &rid, const std::string &attributeName, void *data)
+{
     return -1;
 }
 
 RC RecordBasedFileManager::scan(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
                                 const std::string &conditionAttribute, const CompOp compOp, const void *value,
-                                const std::vector<std::string> &attributeNames, RBFM_ScanIterator &rbfm_ScanIterator) {
+                                const std::vector<std::string> &attributeNames, RBFM_ScanIterator &rbfm_ScanIterator)
+{
     return -1;
 }
-
-
-
