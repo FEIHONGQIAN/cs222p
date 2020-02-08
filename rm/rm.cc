fd7c117e456c2a4bbab27d4c43bc068c0dc465a6
Feihong Qian <feihongq@uci.edu>
2020-02-07 22:13:32 -0800

    test-again-again

--- rm/rm.cc
+++ rm/rm.cc
@@ -315,7 +315,7 @@ RC RelationManager::getAttributes(const std::string &tableName, std::vector<Attr
     free(column);
     free(table_name);
     rbfm->closeFile(fileHandle);
-    //rbfm->closeFile(fileHandleForTable);
+    rbfm->closeFile(fileHandleForTable);
 
     return success;
 }
